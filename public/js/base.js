d37d858565b0a1be2001a847e6f207e22ac28732
高阳 <393724605@qq.com>
2015-06-24 10:22:38 +0800

    post

--- public/js/base.js
+++ public/js/base.js
@@ -83,14 +83,15 @@ $(document).ready(function() {
   });
 
   $('body').on('click', '.show-commend', function() {
-    var ds_loaded = false;
-    window.disqus_shortname = $('.show-commend').attr('name');
-    $.ajax({
-      type: "GET",
-      url: "http://" + disqus_shortname + ".disqus.com/embed.js",
-      dataType: "script",
-      cache: true
-    });
+      var duoshuoQuery = {short_name:"maydaycoder"};
+	(function() {
+		var ds = document.createElement('script');
+		ds.type = 'text/javascript';ds.async = true;
+		ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+		ds.charset = 'UTF-8';
+		(document.getElementsByTagName('head')[0] 
+		 || document.getElementsByTagName('body')[0]).appendChild(ds);
+	})();
   });
   content_effects();
 });
