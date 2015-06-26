9882a144038e0a29ec74e56af588a6d3a9766f17
高阳 <393724605@qq.com>
2015-06-26 09:25:06 +0800

    post

--- public/js/base.js
+++ public/js/base.js
@@ -85,10 +85,7 @@ $(document).ready(function() {
   $('body').on('click', '.show-commend', function() {
     $.ajax({
       type: "GET",
-      data:{
-          uid : 2042092
-      }，
-      url: "http://v2.uyan.cc/code/uyan.js",
+      url: "http://v2.uyan.cc/code/uyan.js?uid=2042092",
       dataType: "script",
       cache: true
     });
@@ -96,4 +93,3 @@ $(document).ready(function() {
   content_effects();
 });
 
-window._bd_share_config={"common":{"bdSnsKey":	{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"24"},"share":{},"image":	{"viewList":["qzone","tsina","tqq","renren","weixin"],"viewText":"分享到：","viewSize":"16"},"selectShare":	{"bdContainerClass":null,"bdSelectMiniList":["qzone","tsina","tqq","renren","weixin"]}};with(document)0	[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
