d39019035c2303668820f10405e27d16ea5bfdc7
高阳 <393724605@qq.com>
2015-07-12 19:16:23 +0800

    post

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,16 +35,6 @@ $(document).ready(function() {
 			                    }
 			      );
 			      
-			      	var duoshuoQuery = {short_name:"maydaycoder"};
-					(function() {
-						var ds = document.createElement('script');
-						ds.type = 'text/javascript';ds.async = true;
-						ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
-						ds.charset = 'UTF-8';
-						(document.getElementsByTagName('head')[0] 
-						 || document.getElementsByTagName('body')[0]).appendChild(ds);
-					})();
-			      
 	  });
 
        $("li.select").click(function(){
