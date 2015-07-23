fbd5c4f83afea011fe0331c9b448468babf9941b
BladeMaster <gycoderhh@gmail.com>
2015-07-23 08:51:02 +0800

    post

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,11 +72,11 @@ $(document).ready(function() {
 	  
 	 $('li.guanyu').click(function() {
 
-        $('.skillbar').each(function() {
+        $('div.skillbar').each(function() {
             $(this).find('.skillbar-bar').width(0);
         });
 
-        $('.skillbar').each(function() {
+        $('div.skillbar').each(function() {
             $(this).find('.skillbar-bar').animate({
                 width: $(this).attr('data-percent')
             }, 2000);
