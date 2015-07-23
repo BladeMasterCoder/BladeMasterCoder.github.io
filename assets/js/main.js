8ea8c18a0fa990d4558d61ec56c7c7f90d89f635
BladeMaster <gycoderhh@gmail.com>
2015-07-23 08:45:51 +0800

    post

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,74 @@
 $(document).ready(function() {
+		
+	$(function () {
+		$(window).scroll(function(){
+			if ($(window).scrollTop()>100){
+				$("#backtotop").addClass("showme");
+			}
+			else
+			{
+				$("#backtotop").removeClass("showme");
+			}
+		});
+		
+		$("#backtotop").click(function(){
+			$('body,html').animate({scrollTop:0},400);
+				return false;
+		});
+		
+		$("pre").addClass("prettyprint linenums");
+    	prettyPrint();
+		
 
+    		
+	});
+
+	
+
+	
+	$(document).pjax('.pjaxlink', '#pjax', {
+	    fragment: "#pjax",
+	    timeout: 10000
+	  });
+	    
+	 $(document).on('pjax:complete', function() {
+		             $('.bookpiclist .bookpic').hover(
+			                    function() {
+			                        $(this).find('.booklabel').stop().animate({bottom: 0}, 200);
+			                        $(this).find('img').stop().animate({top: -30}, 500);
+			                    },
+			                    function() {
+			                        $(this).find('.booklabel').stop().animate({bottom: -40}, 200);
+			                        $(this).find('img').stop().animate({top: 0}, 300);
+			                    }
+			      );
+			      
+	  });
+
+    $("li.select").click(function(){
+				$("li.select").removeClass("active");
+				$(this).addClass("active");
+	});
+
+       $("li.phoneselect").click(function(){
+				$("div.navbar-collapse").removeClass("in");
+				$("button.navbar-toggle").addClass("collapsed");
+	});
+       
+     $(".circle").load(function(){
+	    $(".circle").addClass("show");
+	 });   	
+
+ 	$('.bookpiclist .bookpic').hover(
+                    function() {
+                        $(this).find('.booklabel').stop().animate({bottom: 0}, 200);
+                        $(this).find('img').stop().animate({top: -30}, 500);
+                    },
+                    function() {
+                        $(this).find('.booklabel').stop().animate({bottom: -40}, 200);
+                        $(this).find('img').stop().animate({top: 0}, 300);
+                    }
+      );
 	  
 	 $('li.guanyu').click(function() {
 
