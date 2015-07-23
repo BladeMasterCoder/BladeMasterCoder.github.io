e4c831f2f7e33a1759eee105c1d5eccb7b80fbdc
BladeMaster <gycoderhh@gmail.com>
2015-07-23 08:26:17 +0800

    post

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,18 +19,7 @@ $(document).ready(function() {
 		$("pre").addClass("prettyprint linenums");
     	prettyPrint();
 		
-		$('li.guanyu').click(function() { 
-			$('.skillbar').each(function() {
-				$(this).find('.skillbar-bar').width(0);
-			});
 
-			$('.skillbar').each(function() {
-				$(this).find('.skillbar-bar').animate({
-					width: $(this).attr('data-percent');
-				}, 2000);
-			});
-
-		});
     		
 	});
 
