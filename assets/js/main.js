3b3fe73c4ca2c92dca709745cc4afdf8fb5aa2db
BladeMaster <gycoderhh@gmail.com>
2015-07-23 08:40:12 +0800

    post

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,74 +1,19 @@
 $(document).ready(function() {
-		
-	$(function () {
-		$(window).scroll(function(){
-			if ($(window).scrollTop()>100){
-				$("#backtotop").addClass("showme");
-			}
-			else
-			{
-				$("#backtotop").removeClass("showme");
-			}
-		});
-		
-		$("#backtotop").click(function(){
-			$('body,html').animate({scrollTop:0},400);
-				return false;
-		});
-		
-		$("pre").addClass("prettyprint linenums");
-    	prettyPrint();
-		
 
-    		
-	});
+	  
+	 $('li.guanyu').click(function() {
 
-	
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').width(0);
+        });
 
-	
-	$(document).pjax('.pjaxlink', '#pjax', {
-	    fragment: "#pjax",
-	    timeout: 10000
-	  });
-	    
-	 $(document).on('pjax:complete', function() {
-		             $('.bookpiclist .bookpic').hover(
-			                    function() {
-			                        $(this).find('.booklabel').stop().animate({bottom: 0}, 200);
-			                        $(this).find('img').stop().animate({top: -30}, 500);
-			                    },
-			                    function() {
-			                        $(this).find('.booklabel').stop().animate({bottom: -40}, 200);
-			                        $(this).find('img').stop().animate({top: 0}, 300);
-			                    }
-			      );
-			      
-	  });
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').animate({
+                width: $(this).attr('data-percent')
+            }, 2000);
+        });
 
-    $("li.select").click(function(){
-				$("li.select").removeClass("active");
-				$(this).addClass("active");
-	});
-
-       $("li.phoneselect").click(function(){
-				$("div.navbar-collapse").removeClass("in");
-				$("button.navbar-toggle").addClass("collapsed");
-	});
-       
-     $(".circle").load(function(){
-	    $(".circle").addClass("show");
-	 });   	
-
- 	$('.bookpiclist .bookpic').hover(
-                    function() {
-                        $(this).find('.booklabel').stop().animate({bottom: 0}, 200);
-                        $(this).find('img').stop().animate({top: -30}, 500);
-                    },
-                    function() {
-                        $(this).find('.booklabel').stop().animate({bottom: -40}, 200);
-                        $(this).find('img').stop().animate({top: 0}, 300);
-                    }
-      );
+    });
 	        
 	 
 });
