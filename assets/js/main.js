5b719c3b780eb23975dcbf85c7c810a208d5c7c6
BladeMaster <gycoderhh@gmail.com>
2015-07-30 09:18:48 +0800

    post

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,7 +80,8 @@ $(document).ready(function() {
         }, 2000);
     });    
 
-	$('.navbar-wrapper').stickUp();
-	 
+	$('.navbar-wrapper').stickUp(){
+				$(this).addClass("isStickup");
+	});
 });
 		
