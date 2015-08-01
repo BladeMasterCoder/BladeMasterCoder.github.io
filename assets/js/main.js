8d2cdb4926136015d5eb27b438c0633b091c2f5d
高阳 <393724605@qq.com>
2015-08-01 15:25:09 +0800

    post

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,16 +32,16 @@ $(document).ready(function() {
 	    timeout: 10000
 	  });
 	    
-	/*
+	
 	$(document).on('pjax:send', function() { //pjax链接点击后显示加载动画；
 	    $(".pjax_loading").css("display", "block");
 	});
-	*/
+	
 	 $(document).on('pjax:complete', function() {
 	 	 		$("pre").addClass("prettyprint linenums");
     				prettyPrint();
 	 	 		pajx_loadDuodsuo();//pjax加载完成之后调用重载多说函数
-	 	 	//	$(".pjax_loading").css("display", "none");
+	 	 		$(".pjax_loading").css("display", "none");
 	 	 		window._bd_share_main.init();
 		             $('.bookpiclist .bookpic').hover(
 			                    function() {
