076affa3a64a502bb3d54ea3fad6a508b8363b24
BladeMaster <gycoderhh@gmail.com>
2015-09-24 23:22:06 +0800

    post

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,9 +47,10 @@ $(document).ready(function() {
 	
 	 $(document).on('pjax:complete', function() {
 	 	 		$("pre").addClass("prettyprint linenums");
-    				prettyPrint();
-    				skillbar();
-	 	 		pajx_loadDuodsuo();//pjax加载完成之后调用重载多说函数
+    			prettyPrint();
+    			skillbar();
+	 	 		pajx_loadDuoshuo();//pjax加载完成之后调用重载多说函数
+				pjax_loadChart();
 	 	 		$(".pjax_loading").css("display", "none");
 	 	 		window._bd_share_main.init();
 		             $('.bookpiclist .bookpic').hover(
@@ -89,7 +90,7 @@ $(document).ready(function() {
 	skillbar();
 });
 		
-function pajx_loadDuodsuo(){   
+function pajx_loadDuoshuo(){   
     var dus=$('.ds-thread');
     if($(dus).length==1){
         var el = document.createElement('div');
@@ -98,6 +99,86 @@ function pajx_loadDuodsuo(){
         DUOSHUO.EmbedThread(el);
         $(dus).html(el);
     }
+}
+
+function pjax_loadChart(){
+		require.config({
+            paths: {
+                echarts: 'http://echarts.baidu.com/build/dist'
+            }
+        });
+        
+        // 使用
+        require(
+            [ 
+				'echarts',
+                'echarts/chart/pie' // 使用柱状图就加载bar模块，按需加载
+            ],
+            function (ec) {
+                // 基于准备好的dom，初始化echarts图表
+                var myChart = ec.init(document.getElementById('main')); 
+                
+                var option = {
+				    title : {
+				        text: 'IT技能属性表',
+				        subtext: '',
+				        x:'center'
+				    },
+				    tooltip : {
+				        trigger: 'item',
+				        formatter: "{a} <br/>{b} : {c} ({d}%)"
+				    },
+				    legend: {
+				        orient : 'vertical',
+				        x : 'left',
+				        data:['java','javascript','html','css','c++','data mining']
+				    },
+				    toolbox: {
+				        show : false,
+				        feature : {
+				            mark : {show: true},
+				            dataView : {show: true, readOnly: false},
+				            magicType : {
+				                show: true, 
+				                type: ['pie', 'funnel'],
+				                option: {
+				                    funnel: {
+				                        x: '25%',
+				                        width: '50%',
+				                        funnelAlign: 'left',
+				                        max: 80
+				                    }
+				                }
+				            },
+				            restore : {show: true},
+				            saveAsImage : {show: true}
+				        }
+				    },
+				    calculable : true,
+				    series : [
+				        {
+				            name:'IT技能',
+				            type:'pie',
+				            radius : '55%',
+				            center: ['50%', '60%'],
+				            data:[
+				                {value:90, name:'java'},
+				                {value:60, name:'javascript'},
+				                {value:65, name:'html'},
+				                {value:65, name:'css'},
+				                {value:70, name:'c++'},
+				                {value:60, name:'data mining'}
+				            ]
+				        }
+				    ]
+				};
+                    
+                    
+        
+                // 为echarts对象加载数据 
+                myChart.setOption(option); 
+            }
+        );
 }
 
 
